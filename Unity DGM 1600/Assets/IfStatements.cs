1998eb88f07a17667eb9a6bbf05421e1fbb34ece
Crane Bro's <skylerc15@SkylerCranesMBP.ad.uvu.edu>
2015-09-16 09:16:17 -0600

    IfStatement Assignment

---
+++ Unity DGM 1600/Assets/IfStatements.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour 
+{
+
+	int DenverBroncos = 27;
+	int KansasChiefs = 16;
+
+	void Start()
+	{ 
+		if (DenverBroncos < KansasChiefs)
+		{	
+			Debug.Log ("loss");
+
+		} else if (DenverBroncos > KansasChiefs)
+
+		{
+			Debug.Log ("Win!");
+		} 
+
+		else
+		{
+			Debug.Log ("Tie");
+		}
+	}
+
+}
