58cf35e9749881bee7cae16921111d8bef75014e
Crane Bro's <skylerc15@SkylerCranesMBP.ad.uvu.edu>
2015-09-23 10:50:11 -0600

    Loops

---
+++ Unity DGM 1600/Assets/Loops.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour 
+{
+
+	//While Loop 
+
+	int ShotsMade = 4;
+
+	void Start()
+	{
+		whilelooptest ();
+		dowhilelooptest ();
+		forlooptest ();
+		foreachlooptest ();
+	}
+
+	void whilelooptest()
+	{
+		while (ShotsMade > 0)
+		{
+			Debug.Log ("You've Made a Shot");
+			ShotsMade--;
+		}
+
+	}
+
+
+
+	// Do while Loop
+
+	void dowhilelooptest ()
+	{
+		bool Broncoswin = false;
+
+		do 
+		{ 
+			print ("Broncos Win!");
+		} while (Broncoswin == true);
+	
+	}
+
+	//For Loop
+
+	int NumberofWins = 13;
+
+	void forlooptest()
+	{
+		for (int Wins = 0; Wins < NumberofWins; Wins++) 
+		{
+			Debug.Log ("Denver Broncos earn Victory Number " + Wins); 
+		}	 
+
+	} 
+
+
+	//foreach Loop
+
+	void foreachlooptest ()
+	{
+		string[] Weapons = new string [3];
+
+		Weapons [0] = "sword";
+		Weapons [1] = "bow";
+		Weapons [2] = "axe";
+
+		foreach (string item in Weapons) 
+		{
+			Debug.Log (item);
+		}
+	}
+}
+
+
