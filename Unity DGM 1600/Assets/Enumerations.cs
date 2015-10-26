817b3689da2b5645ea43ad03b4e646522d5a814e
Crane Bro's <skylerc15@SkylerCranesMBP.ad.uvu.edu>
2015-10-26 09:48:13 -0600

    Enumérations assignent

---
+++ Unity DGM 1600/Assets/Enumerations.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enumerations : MonoBehavior 
+{
+
+	enum FootballPlayer {Player1, Player2, Player3, Player4};
+
+	
+	void Start () 
+	{
+		FootballPlayer myplayer; 
+		
+		myplayer = FootballPlayer.Player1;
+	
+	}
+	
+	FootballPlayer reversefootballplayer (FootballPlayer foot)
+	{
+		if(foot == FootballPlayer.Player1)
+			foot= FootballPlayer.Player2;
+		
+		return foot;		
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
