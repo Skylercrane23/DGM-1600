88408b7493192d1c5baae13b9dd948da03eb4375
Crane Bro's <skylerc15@SkylerCranesMBP.ad.uvu.edu>
2015-11-02 09:27:03 -0700

    Switch Statement assignment

---
+++ Unity DGM 1600/Assets/SwtichStatements.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwtichStatements : MonoBehaviour 
+{
+	public int level = 3;
+	
+	void Start ()
+	{
+		LevelUp();
+	}
+	
+	void LevelUp ()
+	{
+		switch (level)
+		{
+			case 3: 
+				print ("You are  now level 3!");
+				break;
+			case 2:
+				print ("You are now level 2!");
+				break;
+			case 1:
+				print ("You are now level 1!");
+				break; 
+			default:
+				print ("You have no level!");
+				break;			
+		}
+	}
+	
+		
+}
